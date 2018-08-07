4a63fe1480e1b963dc6ec398426c090de4f341f7
Dxh7788 <anan1215>
2018-08-07 11:54:47 +0800

    线程池

--- src/main/java/com/diro/main/DiroApplication.java
+++ src/main/java/com/diro/main/DiroApplication.java
@@ -1,13 +1,31 @@
 package com.diro.main;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  *
  */
 public class DiroApplication
 {
+    /*
+    * 有5类
+    * */
+    private final static ExecutorService cachedService = Executors.newCachedThreadPool();
+    private final static ExecutorService fixedService = Executors.newFixedThreadPool(5);
+    private final static ExecutorService scheduledService = Executors.newScheduledThreadPool(5);
+    private final static ExecutorService singleThreadService = Executors.newSingleThreadExecutor();
+    private final static ExecutorService singleThreadscheduledService = Executors.newSingleThreadScheduledExecutor();
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        cachedService.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                System.out.println("hello World");
+                return null;
+            }
+        });
     }
 }
