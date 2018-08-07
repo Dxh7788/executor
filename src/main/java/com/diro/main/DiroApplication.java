a4e551e2de5fe4a9c461efbb31b8ef4452742c22
Dxh7788 <anan1215>
2018-08-07 11:37:39 +0800

    修改

---
+++ src/main/java/com/diro/main/DiroApplication.java
@@ -0,0 +1,13 @@
+package com.diro.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class DiroApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
